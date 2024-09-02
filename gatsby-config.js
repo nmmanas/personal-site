1d0b90d7300a2428fbb038d84c1255d9a649a1b0
Manas <nmmanas@gmail.com>
2024-09-02 20:38:03 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Manas Najmuddeen`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
